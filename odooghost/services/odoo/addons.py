aa4566c8fb93f5693a09fade67ce0703402a4fb9
Remy Zulauff <remy.zulauff@gmail.com>
2025-11-17 13:43:32 +0100

    fix(addons): When addons is local do not  try to get git repo

--- odooghost/services/odoo/addons.py
+++ odooghost/services/odoo/addons.py
@@ -53,6 +53,9 @@ class AddonsHandler:
         """
         addons_path = []
         for addon in self._get_addons():
+            if addon.type != "remote":
+                addons_path.append(addon.container_posix_path)
+                continue
             path = addon.path or self.get_context_path(addon)
             repo = Repo(path.as_posix())
             if not repo.submodules:
